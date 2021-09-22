791a464640a3a83d4ca5133655ffa3a93691a5ad
Ash-sungho <andykr1234@gmail.com>
2021-09-22 22:52:46 +0900

    Javascript : Transparent home

--- Javascript/main.js
+++ Javascript/main.js
@@ -4,11 +4,40 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 //Make navbar transparent when it is on the top
-document.addEventListener('scroll',() =>{
-console.log(navbarHeight);
-if(window.scrollY > navbarHeight){
-    navbar.classList.add('navbar--dark');
-}else{
-    navbar.classList.remove('navbar--dark');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    scrollIntoViews(link);
+
+});
+//Handle scrolling when tapping on the contact button
+const contact_btn = document.querySelector('.home__contact');
+contact_btn.addEventListener('click', (e) => {
+    const link =e.target.dataset.link;
+    scrollIntoViews(link);
+});
+
+function scrollIntoViews(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
 }
+//Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll',() => {
+    home.style.opacity = 1 - window.scrollY /homeHeight;
 });
