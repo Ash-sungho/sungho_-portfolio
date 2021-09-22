0a4367501fee5613a33951e794d1337abf152791
Ash-sungho <andykr1234@gmail.com>
2021-09-22 23:43:22 +0900

    Javascript : arrow-up

--- Javascript/main.js
+++ Javascript/main.js
@@ -30,10 +30,6 @@ contact_btn.addEventListener('click', (e) => {
     scrollIntoViews(link);
 });
 
-function scrollIntoViews(selector) {
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
-}
 //Make home slowly fade to transparent as the window scrolls down
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
@@ -41,3 +37,22 @@ const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll',() => {
     home.style.opacity = 1 - window.scrollY /homeHeight;
 });
+
+//Show "arrow up" button when scrolling
+const arrowup = document.querySelector('.arrow-up');
+document/addEventListener('scroll',()=>{
+    if(window.scrollY> homeHeight/2){
+         arrowup.classList.add('visible');
+         arrowup.style.opacity =  window.scrollY/window.outerHeight -1;
+    }else{
+        arrowup.classList.remove('visible');
+    }
+});
+//Handle click on the "arrow up" button
+arrowup.addEventListener('click',()=>{
+    scrollIntoViews('#home');
+});
+function scrollIntoViews(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+}
